81537ed08cc0414e32af4856ccd122759d72040c
Manthan Kansara <manthankansara7@gmail.com>
2024-07-03 11:29:10 +0100

    Fixed speaker_diarization code for testing

--- speaker_diarization.py
+++ speaker_diarization.py
@@ -1,11 +1,16 @@
 from pyannote.audio import Pipeline
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 
 pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization@2.1", use_auth_token=AUTH_TOKEN
 )
 
 # apply the pipeline to an audio file
-diarization = pipeline("Sample.wav")
-
+diarization = pipeline("./images/Sample.wav")
+print("Diarization is completed.")
 print(diarization)

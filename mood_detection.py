8148878fe6a79d428dd526008c37754023bd53fe
Manthan Kansara <manthankansara7@gmail.com>
2024-07-04 13:03:46 +0100

    Fixed emotion recognition function

--- mood_detection.py
+++ mood_detection.py
@@ -108,16 +108,15 @@ class ProjectARIAHandler:
 
 
 class StreamingClientObserver:
-    def __init__(self, image_queue: Queue):
+    def __init__(self):
         self.rgb_image = None
-        self.image_queue = image_queue
         self.face_cascade = cv2.CascadeClassifier(
             cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
         )
         self.image_counter = 0
 
     def on_image_received(self, image: np.array, record: ImageDataRecord):
-        self.image_queue.put(image)
+        self.rgb_image = image
 
     def detect_mood(self):
         """
@@ -126,124 +125,30 @@ class StreamingClientObserver:
         emotion_detector = FER(mtcnn=True)
         while True:
             try:
-                image = self.image_queue.get(timeout=1)  # Wait for 1 second
-                print(image)
+                image = self.rgb_image
+                image = np.rot90(image, -1)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                # print(image)
                 if image is None:  # Termination signal
                     break
-
-                analysis = emotion_detector.detect_emotions(image)
+                face_objs = DeepFace.extract_faces(
+                    img_path=image,
+                    detector_backend=backends[4],
+                    align=alignment_modes[0],
+                )
+                face_image = face_objs[0]["face"]
+                if face_image.dtype != np.uint8:
+                    face_image = (255.0 * face_image).astype(np.uint8)
+                analysis = emotion_detector.detect_emotions(face_image)
+                emotion, score = emotion_detector.top_emotion(face_image)
 
                 if analysis:
-                    print(analysis)
+                    print(emotion, score)
                     continue
 
-                # face_objs = DeepFace.extract_faces(
-                #     img_path=image,
-                #     detector_backend=backends[4],
-                #     align=alignment_modes[0],
-                # )
-
-                # if face_objs is None:
-                #     print("No face detected in the image.")
-                #     continue
-
-                # # Use DeepFace to analyze the image
-
-                # for face_obj in face_objs:
-                #     # print(face_obj)
-                #     print("Face Detected")
-                #     face_image = face_obj["face"]
-                #     # Convert the face image to a format DeepFace expects
-                #     if face_image.dtype != np.uint8:
-                #         face_image = (255.0 * face_image).astype(np.uint8)
-                #         print(face_image)
-                #     analysis = DeepFace.analyze(
-                #         img_path=face_image,
-                #         actions=["emotion"],
-                #         enforce_detection=False
-                #     )
-                #     print(analysis)
-                #     # Extract the dominant emotion and its confidence
-                #     dominant_emotion = analysis["dominant_emotion"]
-                #     confidence = analysis["emotion"][dominant_emotion]
-
-                #     print(
-                #         f"Detected mood: {dominant_emotion} with confidence: {confidence:.2f}%"
-                #     )
-
-            except Empty:
-                continue  # No image in the queue, continue waiting
             except Exception as e:
-                # print(f"Error detecting mood: {e}")
+                print(f"No face detected")
                 continue
-
-    # def preprocess_image(self, image: np.array) -> np.array:
-    #     """
-    #     Preprocesses the image to detect and crop the face, and converts it to RGB.
-
-    #     Args:
-    #     - image (np.array): The original image.
-
-    #     Returns:
-    #     - np.array: The processed image ready for face detection and mood analysis.
-    #     """
-    #     try:
-    #         # Convert to grayscale for face detection
-    #         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    #         # Focus on the central part of the image
-    #         height, width = gray_image.shape
-    #         central_region = gray_image[
-    #             height // 4 : 3 * height // 4, width // 4 : 3 * width // 4
-    #         ]
-
-    #         # Adjust detection parameters for different conditions
-    #         faces = self.face_cascade.detectMultiScale(
-    #             central_region,
-    #             scaleFactor=1.1,
-    #             minNeighbors=5,
-    #             minSize=(30, 30),
-    #             flags=cv2.CASCADE_SCALE_IMAGE,
-    #         )
-
-    #         if len(faces) == 0:
-    #             return None  # No faces detected
-
-    #         # Adjust face coordinates to the original image dimensions
-    #         print(faces[0])
-    #         x, y, w, h = faces[0]
-    #         x += width // 4
-    #         y += height // 4
-
-    #         padding = 100  # Add padding to avoid too-tight cropping
-    #         x = max(0, x - padding)
-    #         y = max(0, y - padding)
-    #         w += 2 * padding
-    #         h += 2 * padding
-
-    #         # Draw a rectangle around the detected face for debugging
-    #         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-    #         # Crop the face
-    #         face_image = image[y : y + h, x : x + w]
-
-    #         # Convert the face image to RGB
-    #         rgb_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB)
-
-    #         # Resize to the required size for the model (e.g., 224x224 or 48x48)
-    #         resized_image = cv2.resize(rgb_image, (48, 48))
-
-    #         # Rotate the image 90 degrees to the right
-    #         rotated_image = cv2.rotate(resized_image, cv2.ROTATE_90_CLOCKWISE)
-
-    #         debug_filename = f"face_detected_{self.image_counter}.jpg"
-    #         cv2.imwrite(debug_filename, rotated_image)
-    #         print(f"Face detected and saved as {debug_filename}")
-
-    #         return rotated_image
-    #     except Exception as e:
-    #         print(f"Error in preprocessing image: {e}")
-    #         return None
 
 
 # Main function to set up and manage streaming
@@ -253,11 +158,10 @@ def main():
     [streaming_client, streaming_manager, device_client, device] = (
         project_aria_handler.setup_project_aria()
     )
-    image_queue = Queue()
     try:
         streaming_manager.start_streaming()
         print(f"Streaming state: {streaming_manager.streaming_state}")
-        observer = StreamingClientObserver(image_queue)
+        observer = StreamingClientObserver()
         streaming_client.set_streaming_client_observer(observer)
 
         print("Start listening to image data")
--- speaker_diarization.py
+++ speaker_diarization.py
@@ -1,16 +1,34 @@
 from pyannote.audio import Pipeline
 from dotenv import load_dotenv
 import os
+from faster_whisper import WhisperModel
 
 load_dotenv()
 
 AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 
+
+def label_transcription(self, diarization, transcription):
+    labeled_segments = []
+    for segment, _, speaker in diarization.itertracks(yield_label=True):
+        start_time = segment.start
+        end_time = segment.end
+        text_segment = self.get_text_segment(transcription, start_time, end_time)
+        labeled_segments.append(f"[{speaker}] {text_segment}")
+    return "\n".join(labeled_segments)
+
+
 pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization@2.1", use_auth_token=AUTH_TOKEN
 )
+model = WhisperModel("tiny.en", device="cpu", compute_type="float32")
+segments, _ = model.transcribe("./images/Sample.wav", beam_size=5, language="en")
+transcription = "\n".join(segment.text for segment in segments)
 
 # apply the pipeline to an audio file
 diarization = pipeline("./images/Sample.wav")
 print("Diarization is completed.")
 print(diarization)
+
+labeled_transcription = label_transcription(diarization, transcription)
+print(labeled_transcription)
